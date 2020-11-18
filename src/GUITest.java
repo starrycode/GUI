2843b3aa3e53b696e0a75daba94b3e876aaa5ab5
Violet <Jiye.Oh-1@ou.edu>
2020-11-18 16:16:48 -0600

    Initial commit

---
+++ src/GUITest.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class GUITest {
+
+	public static void main(String[] args) {
+		// So far, all our program were sequential and
+		// they reached the END - finito
+		JFrame frame = new JFrame("Dark wave");
+		frame.setSize(300, 400);
+
+	}
+
+}
